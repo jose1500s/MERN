a74a19eabb2b32c74aec39153763321f5410d707
Jose Antonio P <120035990@upq.edu.mx>
2024-03-04 18:25:54 -0600

    creacion campos tabla tasks y funcion controlador createTask

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import indexRoutes from './routes/index.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 const app = express();
 
+app.use(express.json())
+
 app.use(indexRoutes)
 app.use(taskRoutes)
 app.listen(PORT, () => {
